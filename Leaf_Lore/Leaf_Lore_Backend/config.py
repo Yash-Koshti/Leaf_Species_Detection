ad71abac225b12c42a4488b6864c40d228e19c25
Yash-Koshti <yashkoshti27@gmail.com>
2024-05-19 20:58:07 +0530

    FIX-LLB: exclude compose file from dockerignore

--- Leaf_Lore/Leaf_Lore_Backend/config.py
+++ Leaf_Lore/Leaf_Lore_Backend/config.py
@@ -12,6 +12,11 @@ load_dotenv()
 if os.getenv("DATABASE_URL") is None:
     raise Exception("DATABASE_URL environment variable is not set")
 
+# database_host = os.getenv("DATABASE_HOST", "db")
+
+# Example URL: postgresql://user:password@host:port/db_name
+# DATABASE_URL = f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{database_host}:5432/{os.getenv("POSTGRES_DB")}"
+
 # Create the database engine
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
